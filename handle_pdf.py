333c73df3bc2a6567629c2720adf428a31ea5b56
linzhanpeng <jiuyanduhuo@hotmail.com>
2024-08-02 01:27:00 +0800

    Added total calculate function for pdf invoice

--- handle_pdf.py
+++ handle_pdf.py
@@ -1,6 +1,17 @@
 import os
+import re
+from pathlib import Path
+
 from PyPDF2 import PdfReader, PdfWriter
 import logging
+from PIL import Image
+import io
+from PyPDF2 import PdfFileReader, PdfFileWriter
+import pdf2image
+import pdfplumber
+from decimal import Decimal, getcontext
+
+getcontext().prec = 6
 
 
 def extract_number(filename):
@@ -22,13 +33,32 @@ def get_pdf_files_in_current_path(current_path=os.getcwd()):
     """
     all_items = os.listdir(current_path)
     files = [os.path.join(current_path, f) for f in all_items if
-             f.endswith('.pdf') and (not f.endswith('merged.pdf')) and os.path.isfile(os.path.join(current_path, f))]
+             f.lower().endswith('.pdf') and (not f.endswith('merged.pdf')) and os.path.isfile(os.path.join(current_path, f))]
     if files:
         sorted_files = sorted(files, key=extract_number)
         print(f"all sorted pdf files by number in current path,【{current_path}】:\n {sorted_files}")
         return sorted_files
     else:
         return False
+
+def get_files_in_current_path(current_path=os.getcwd(),suffix = "merged.pdf"):
+    """
+     Get specified type files in current path.
+    :param current_path
+    :return: sorted files if success,otherwise return False.
+    """
+    all_items = os.listdir(current_path)
+    files = [os.path.join(current_path, f) for f in all_items if
+             f.lower().endswith(suffix)  and os.path.isfile(os.path.join(current_path, f))]
+    if files:
+        sorted_files = sorted(files, key=extract_number)
+        print(f"all sorted pdf files by number in current path,【{current_path}】:\n {sorted_files}")
+        return sorted_files
+    else:
+        return False
+
+
+
 
 
 def merge_pdfs(paths, output="_merged.pdf"):
@@ -54,13 +84,15 @@ def merge_pdfs(paths, output="_merged.pdf"):
     return True
 
 
-def find_folders_with_pdfs(folder_path):
+def find_folders_with_pdfs(folder_path,suffix = ".pdf"):
     folders_with_pdfs = []
     # Directory tree generator
     for root, dirs, files in os.walk(folder_path):
-        if any(file.lower().endswith('.pdf') for file in files):
+        if any(file.lower().endswith(suffix) for file in files):
             folders_with_pdfs.append(root)
     return folders_with_pdfs
+
+
 
 
 def merge_all_pdfs():
@@ -79,10 +111,7 @@ def merge_all_pdfs():
         if files:
             merge_pdfs(files, output=folder)
 
-from PIL import Image
-import io
-from PyPDF2 import PdfFileReader, PdfFileWriter
-import pdf2image
+
 
 
 def merge_pdf_pages_to_one(pdf1_path, pdf2_path, output_path):
@@ -109,19 +138,95 @@ def merge_pdf_pages_to_one(pdf1_path, pdf2_path, output_path):
 
     combined_image.save(output_path, "PDF", resolution=100.0)
 
+def contains_uppercase_currency_numbers(s):
+    uppercase_currency_numbers = {'壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖', '拾', '佰', '仟', '万', '亿', '圆','整'}
+    for char in s:
+        if char in uppercase_currency_numbers:
+            return True
+    return False
+
+def extract_cny_numerical_values(text):
+    # match numbers after ¥
+    pattern = r'(?:¥|\s*￥\s*)\s*(\d+\.\d+|\d+)'
+    matches = re.findall(pattern,text)
+    return matches
+
+def calculate_total_price_tax_from_pdf(file):
+    # .一级一级的处理：
+    """
+    一. 把有发票总金额的行找出来。规则如下：
+    1. 包含：价税合计
+    2. 包含：大写数字的，比如：贰佰叁拾壹圆柒角
+    3. 无上述两种，但包含总计的
+    4. 后续待补充
+    """
+    with pdfplumber.open(file) as pdf:
+        price_tax_text = []
+        price_tax_value = []
+        for index, page in enumerate(pdf.pages):
+            page_text = page.extract_text()
+            print(f"\npage:{index + 1}")
+            print(page_text)
+            text_arr = page_text.split('\n')
+            for line in text_arr:
+                # print(line)
+                if ("价税合计" in line) and (( "¥"  in line) | ("￥" in line)):
+                    price_tax_text.append(line)
+                    print(line)
+                    continue
+                if contains_uppercase_currency_numbers(line) & (( "¥"  in line)| ("￥" in line)):
+                    price_tax_text.append(line)
+                    print(line)
+                # if ("总计" in line) & (( "¥"  in line)):
+                #     price_tax_text.append(line)
+                #     print(line)
+
+        print(f"price_tax_text: {price_tax_text}")
+        price_tax_text_set = set(price_tax_text)
+        print(f"price_tax_text_set: {price_tax_text_set}")
+
+        for item in price_tax_text_set:
+            price_tax_value.append(extract_cny_numerical_values(item))
+        print(price_tax_value)
+        sum = Decimal('0')
+        for val in price_tax_value:
+            for v in val:
+                sum += Decimal(v)
+        rounded_result = sum.quantize(Decimal('0.01'))
+        print(f"sum = {sum}")
+        print(f"rounded_result = {rounded_result}")
+
+        return rounded_result
 
 if __name__ == '__main__':
 
     # 1. merge pdfs
     # merge_all_pdfs()
 
-    # 2. 两页PDF合并到1页
-    pdf1_path = '1.pdf'
-    pdf2_path = '2.pdf'
-    # 输出文件路径
-    output_path = 'merged_page.pdf'
+    # # 2. 两页PDF合并到1页
+    # pdf1_path = '1.pdf'
+    # pdf2_path = '2.pdf'
+    # # 输出文件路径
+    # output_path = 'merged_page.pdf'
+    #
+    # # 调用函数合并PDF页面内容
+    # merge_pdf_pages_to_one(pdf1_path, pdf2_path, output_path)
 
-    # 调用函数合并PDF页面内容
-    merge_pdf_pages_to_one(pdf1_path, pdf2_path, output_path)
+    folders = find_folders_with_pdfs(os.getcwd())
+    for folder in folders:
+        print(folder)
+        files = get_files_in_current_path(current_path=folder)
+        for file in files:
+            print(file)
+            total = calculate_total_price_tax_from_pdf(file)
+            print(f"file:{file} total:{total}")
+            file_name = os.path.basename(file)
+            result_path = Path(folder).joinpath(f"{file_name}_total_¥{total}.txt")
+            with open(f"{file_name}_total_¥{total}.txt",'w') as f:
+                f.write(f"{file}\ntotal: ¥{total}")
+            with open(result_path,'w') as f:
+                f.write(f"{file}\ntotal: ¥{total}")
+
+
 
 
